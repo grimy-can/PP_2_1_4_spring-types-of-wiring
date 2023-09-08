6b81f524c69f2bc7b60beb67b7bf473f31d42d8d
Grimy Can <greyhead@bk.ru>
2023-09-08 20:51:53 +0500

    задача 2.1.5 решение 1

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,15 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    private final String DETH = "";
+
+
+    @Override
+    public String toString() {
+        return DETH;
+    }
 }
